d9a56abcf8ff38b1c0fef2ee668c228473a9b301
kochjar <timgfx@outlook.com>
2023-04-19 14:30:05 +0200

    Done

--- HuseByg/Program.cs
+++ HuseByg/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Text;
+using HuseByg.controller;
+using HuseByg.model;
 
 namespace HuseByg
 {
@@ -12,31 +14,54 @@ namespace HuseByg
 
             Controller controller = new Controller();
 
-            controller.TilføjHus("Søndergade 30, 8000 Aarhus");
-            controller.TilføjLejer("Jens Hansen", "1234567890", "10203040","Jenshansen@gmail", 1, "H1");
-            controller.TilføjLejemål(DateTime.Parse("01-01-2019"), DateTime.Parse("01-01-2023"), "L1");
+            controller.TilføjHus("Søndergade 20, 8000 Aarhus", HusType.Lille, 60, 2);
+            controller.TilføjLejer("Mike Oxlong", "1234567890", " mikeoxlong@gmail.com");
+            controller.TilføjLejer("Wilma Diqfit", "2345678901", "wilma.diqfit@gmail.com");
+            controller.TilføjLejemål(new DateTime(2019, 1, 1), new DateTime(2022, 1, 1), 15000, 1, 0, "H1", "L1", "L2");
 
-            controller.TilføjHus("Roskildevej 20, 4000 Roskilde");
-            controller.TilføjLejer("Mette Nielsen", "2345678901", "20304050", "Mettenielsen@gmail", 2, "H2");
-            controller.TilføjLejemål(DateTime.Parse("01-02-2020"), DateTime.Parse("01-02-2024"), "L2");
+            controller.TilføjHus("Østervej 11A, 6000 Kolding", HusType.Ende, 80, 3);
+            controller.TilføjLejer("Heywood Jablowme", "1234567890", "heywood.jablowme@gmail.com");
+            controller.TilføjLejemål(new DateTime(2019, 5, 1), new DateTime(2022, 1, 1), 25000, 0, 5, "H2", "L3");
 
-            controller.TilføjHus("Bredgade 10, 5000 Odense");
-            controller.TilføjLejer("Peter Jensen", "3456789012", "30405060", "Peterjensen@gmail", 3, "H3");
-            controller.TilføjLejemål(DateTime.Parse("01-03-2021"), DateTime.Parse("01-03-2025"), "L3");
+            controller.TilføjHus("Østervej 11B, 6000 Kolding", HusType.Stor, 80, 3);
+            controller.TilføjLejer("Hugh G. Rection", "1234567890", "hugh.g.rection@gmail.com");
+            controller.TilføjLejer("Gabe Itch", "1234567890", "gabe.itch@gmail.com");
+            controller.TilføjLejemål(new DateTime(2019, 1, 1), new DateTime(2022, 1, 1), 27000, 2, 0, "H3", "L4", "L5");
 
-            controller.TilføjHus("Nørrebrogade 5, 2200 København");
-            controller.TilføjLejer("Sofie Larsen", "4567890123", "40506070", "Sofielarsen@gmail", 4, "H4");
-            controller.TilføjLejemål(DateTime.Parse("01-04-2022"), DateTime.Parse("01-04-2026"), "L4");
+            controller.TilføjHus("Østervej 11C, 6000 Kolding", HusType.Stor, 85, 4);
 
-            controller.TilføjHus("Åboulevarden 15, 8000 Aarhus");
-            controller.TilføjLejer("Anders Nielsen", "5678901234", "50607080", "Andersnielsen@gmail", 5, "H5");
-            controller.TilføjLejemål(DateTime.Parse("01-05-2023"), DateTime.Parse("01-05-2027"), "L5");
-            
-            controller.TilføjHus("Roskildevej 32, 4000 Roskilde");
-            controller.TilføjHus("Bredgade 19, 5000 Odense");
 
-            controller.VisOversigt();
+            List<Hus> huse = controller.HentAlleHuse();
+            List<Lejer> lejere = controller.HentAlleLejere();
+            List<Lejemål> lejemåls = controller.HentAlleLejemål();
 
+            foreach (Hus hus in huse)
+            {
+                Console.WriteLine($"{hus.HusId}: {hus.Adresse}");
+                if (hus.Lejemål != null)
+                {
+                    Console.WriteLine($"  Indbetalt Depositum: {hus.Lejemål.IndbetaltDepositum}, Hund: {hus.Lejemål.AntalHunde}, Katte: {hus.Lejemål.AntalKatte}");
+                    foreach (Lejer lejer in hus.Lejemål.Lejere)
+                    {
+                        Console.WriteLine($"  {lejer.LejerId}: {lejer.navn}, {lejer.mail}");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+
+
+
+            /*
+            foreach (Lejemål lejemål in lejemåls)
+            {
+                foreach (Lejer lejer in lejemål.Lejere) 
+                {
+                    Console.WriteLine(lejer.navn);
+                    Console.WriteLine(lejer.mail);
+                }
+            }
+            */
         }
     }
 }
